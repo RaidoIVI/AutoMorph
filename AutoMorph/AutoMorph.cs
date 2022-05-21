ebce25b60b224d679824bac03f24ba580b027b56
Иван Жеребятьев <kiba82@gmail.com>
2022-05-21 10:43:30 +0300

    Переделано в BigInt

--- AutoMorph/AutoMorph.cs
+++ AutoMorph/AutoMorph.cs
@@ -1,17 +1,19 @@
-﻿namespace AutoMorph
+﻿using System.Numerics;
+
+namespace AutoMorph
 {
     internal static class AutoMorph
     {
-        private static readonly List<ulong> foundNumber = new List<ulong>(100);
-        private static ulong exponent;                                                //текущий порядок числа
+        private static readonly List<BigInteger> foundNumber = new List<BigInteger>(100);
+        private static BigInteger exponent;                                                //текущий порядок числа
         private static int addCount;                                                  //сколько добавлено чисел в прошлом порядке
         private static int counter;                                                   //счетчик текущего порядка
 
-        private static void Generation(ulong value)
+        private static void Generation(BigInteger value)
         {
-            for (ulong i = 0; i < 10; i++)
+            for (BigInteger i = 0; i < 10; i++)
             {
-                ulong newValue = (i * exponent) + value;
+                BigInteger newValue = (i * exponent) + value;
                 if (NumberTest(newValue))
                 {
                     foundNumber.Add(newValue);
@@ -24,21 +26,21 @@
         {
             var tmp = foundNumber.Count();
 
-                for (int i = addCount; i > 0; i--)
-                {
-                    Generation(foundNumber[tmp-i]);
-                }
+            for (int i = addCount; i > 0; i--)
+            {
+                Generation(foundNumber[tmp - i]);
+            }
 
-            exponent = exponent*10;
+            exponent = exponent * 10;
             addCount = counter;
             counter = 0;
         }
 
-        private static bool NumberTest(ulong value)
+        private static bool NumberTest(BigInteger value)
         {
 
-            ulong tmp = value * value;
-            tmp = tmp - ((tmp / (10*exponent)) * (10*exponent));
+            BigInteger tmp = value * value;
+            tmp = tmp - ((tmp / (10 * exponent)) * (10 * exponent));
             if (tmp == value)
             {
                 return true;
@@ -49,24 +51,24 @@
             }
         }
 
-        internal static void Run()
+        internal static void Run(BigInteger MaxValue)
         {
             foundNumber.Add(1);
             addCount = 1;
             exponent = 1;
             counter = 0;
 
-            while (exponent < 100000000000)
+            while (exponent < MaxValue)
             {
                 TestExponent();
             }
         }
 
-        internal static List<ulong> Get()
+        internal static List<BigInteger> Get()
         {
             foundNumber.Sort();
-            List<ulong> result = new List<ulong>();
-            foreach (ulong value in foundNumber)
+            List<BigInteger> result = new List<BigInteger>();
+            foreach (BigInteger value in foundNumber)
             {
                 if (!result.Contains(value))
                 {
--- AutoMorph/Program.cs
+++ AutoMorph/Program.cs
@@ -1,25 +1,31 @@
 ﻿using System.Diagnostics;
+using System.Numerics;
+
 namespace AutoMorph
 {
     class Program
     {
         static void Main()
         {
-
+            BigInteger MaxValue = 10000000000000000000;
+            MaxValue *= MaxValue;
+            MaxValue *= MaxValue;
+            MaxValue *= MaxValue;
+            MaxValue *= MaxValue;
             Console.ReadKey();
-            Console.WriteLine($"Начинаем вычисления");
+            Console.WriteLine($"Начинаем вычисления до {MaxValue}");
             var Timer = Stopwatch.StartNew();
 
-            AutoMorph.Run();
+            AutoMorph.Run(MaxValue);
 
             Timer.Stop();
             Console.WriteLine($"Вычисления закончены за {Timer.Elapsed.TotalSeconds} секунд");
 
             foreach (var item in AutoMorph.Get())
             {
-                Console.WriteLine(item);
+                Console.WriteLine("{0,200}\n{1,200}",item,item*item);
             }
-
+            Console.WriteLine($"Найдено {AutoMorph.Get().Count} значений");
             Console.ReadKey();
         }
     }
